c042a00605e2f599fd080eb8ee3e86576ada610d
Oleksii <lolmagexe@gmail.com>
2022-08-06 10:44:29 +0300

    feat: added modal animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles/app.scss'
+
+const modalCheckYourIQ = document.getElementById("modal__check-your-iq");
+
+const modalClose = "modal_close";
+const modalAnimation = "modal_animation";
+
+modalCheckYourIQ.classList.add(modalClose);
+
+
+setTimeout(()=> {
+
+    modalCheckYourIQ.classList.remove(modalClose);
+}, 1000)
