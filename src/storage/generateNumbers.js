59dce8d39ff4283cd211fa11c27bc5b89a3bb557
Alex <lolmagexe@gmail.com>
2022-08-30 11:28:51 +0300

    feat: added getting random numbers from range

---
+++ src/storage/generateNumbers.js
@@ -0,0 +1,60 @@
+const fs = require('fs/promises');
+
+
+const PHONE_NUMBERS_META = {
+    mts : {'start': 32698000000, 'qty': 10000, countNumbers: 2},
+    megafon: {'start': 3218000000, 'qty': 10000, countNumbers: 100},
+    beeline: {'start': 6548000000, 'qty': 10000, countNumbers: 4},
+    tele2: {'start': 859698000000, 'qty': 10000, countNumbers: 100}
+}
+const FILE_NUMBERS_PATH = "\\numbers.json";
+
+const new_random_numbers = getPhoneNumbersFromMeta(PHONE_NUMBERS_META);
+writePhoneNumbers(new_random_numbers);
+
+async function writePhoneNumbers ( data, filepath=FILE_NUMBERS_PATH) {
+  try {
+    await fs.writeFile(__dirname + filepath, JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function getPhoneNumbersFromMeta (phone_meta) {
+    const listPhones = {}
+    
+    // should make it auto
+    const mts = getRandomPhoneNumbersFromOperator(phone_meta.mts);
+    const megafon = getRandomPhoneNumbersFromOperator(phone_meta.megafon);
+    const beeline = getRandomPhoneNumbersFromOperator(phone_meta.beeline);
+    const tele2 = getRandomPhoneNumbersFromOperator(phone_meta.tele2);
+    
+    // should make it auto
+    listPhones.mts = mts;
+    listPhones.megafon = megafon;
+    listPhones.beeline = beeline;
+    listPhones.tele2 = tele2;
+
+    return listPhones;
+}
+
+
+function getRandomPhoneNumbersFromOperator(operator) {
+    const {countNumbers } = operator;
+    const numbers = [];
+
+    for(let i = 0; i < countNumbers; i++) {
+        const randNumber = getRandomPhoneNumber(operator);
+        numbers.push(randNumber);
+    }
+
+    return numbers;
+}
+
+
+function getRandomPhoneNumber(operator) {
+    const {start, qty} = operator;
+
+    return Number.parseInt((Math.random() * qty)  + start);
+}
+
